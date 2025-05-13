8ca71f2324777e8a1e5a7d933fd3aeaa01e47626
soham <sahas@terpmail.umd.edu>
2025-05-12 20:26:21 -0400

    fixing favicon

---
+++ forSohamTesting.js
@@ -0,0 +1,27 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const portNumber = 5000;
+const app = express();
+
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname));
+
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+
+
+
+app.listen(portNumber);
+console.log(`Web server started and running at http://localhost:${portNumber}`);
